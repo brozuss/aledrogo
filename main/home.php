02e9b45e1545f806e964dab2d26ab42b13f4a755
brozuss <kacperbrozynski01@gmail.com>
2023-03-19 02:19:35 +0100

    filtering auctions

--- main/home.php
+++ main/home.php
@@ -4,35 +4,107 @@ include('static/head.php');
 include('static/navbar.php');
 ?>
 <!-- filter nav -->
-<nav>
-  <form action="home.php">
-    labe
-  </form>
-</nav>
+<h2>Filtry</h2>
+
+<form action="home.php" method="post">
+  <nav class="filternav">
+    <div class="filter-item">  
+      <label for="kategoria">Kategoria</label>
+          <select name="kategoria" id="kategoria">
+            <option value="0">Wszystkie kategorie</option>
+            <option value="1">Komputery</option>
+            <option value="2">Elektronika</option>
+            <option value="3">Motoryzacja</option>
+          </select>
+    </div>
+    <div class="filter-item">
+      <label for="cena">Cena</label>
+        <div><input type="number" placeholder="Od" name="cena_od">
+        <input type="number" placeholder="Do" name="cena_do"></div>
+    </div>
+    <div class="filter-item">
+      <input type="submit" class="btnfilter" value="FILTRUJ" name="filtersubmit">
+    </div>
+  </nav>
+</form>
+
 
   <!-- Products -->
 <section class="container-all-products">
       <?php
         include('../phpconfig/connect.php');
-        $sel=mysqli_query($connect, "SELECT * FROM `przedmioty`");
-        while($row = mysqli_fetch_array($sel)){
-          $nazwa=$row['nazwa'];
-          $opis=$row['opis'];
-          $img_name=$row['img_name'];
-          $cena=$row['cena_wywolawcza'];
-          echo("
-          <a href='' class='link'><div class='container-product'>
-            <img src='zdjecia/$img_name'>
-            <div class='title_description'>
-              <div class='title'> <h3>$nazwa</h3> </div>
-              <div class='description'> <p>$opis</p> </div>
-            </div>
-            <div class='price'>
-                <p>$cena zł</p>
-            </div>
-          </div></a>
-
-          ");
+        // if(isset($_REQUEST['kategoria']) && isset($_REQUEST['cena_od']) && isset($_REQUEST['cena_do'])){
+        if(isset($_REQUEST['filtersubmit'])){
+          
+          $filtr_kategoria=$_REQUEST['kategoria'];
+          $filtr_cena_od=$_REQUEST['cena_od'];
+          $filtr_cena_do=$_REQUEST['cena_do'];
+          if($_REQUEST['kategoria']==0){
+            $sel=mysqli_query($connect, "SELECT *, licytacje.cena_podbicie FROM `przedmioty` JOIN licytacje ON licytacje.przedmiot_id=przedmioty.id");
+          
+            while($row = mysqli_fetch_array($sel)){
+              $nazwa=$row['nazwa'];
+              $opis=$row['opis'];
+              $img_name=$row['img_name'];
+              $cena_wywolawcza=$row['cena_wywolawcza'];
+              $cena_podbicie=$row['cena_podbicie'];
+              echo("
+              <a href='' class='link'><div class='container-product'>
+                <img src='zdjecia/$img_name'>
+                <div class='title_description'>
+                  <div class='title'> <h3>$nazwa</h3> </div>
+                  <div class='description'> <p>$opis</p> </div>
+                </div>
+                <div class='price'>
+                    <p>$cena_podbicie zł</p>
+                </div>
+              </div></a>
+              ");
+            }
+          }else{
+            $sel=mysqli_query($connect, "SELECT przedmioty.nazwa, przedmioty.opis, przedmioty.img_name, licytacje.cena_podbicie FROM `przedmioty` JOIN licytacje ON licytacje.przedmiot_id=przedmioty.id WHERE przedmioty.kategoria_id=$filtr_kategoria AND licytacje.cena_podbicie BETWEEN $filtr_cena_od AND $filtr_cena_do");
+            
+            while($row = mysqli_fetch_array($sel)){
+              $nazwa=$row['nazwa'];
+              $opis=$row['opis'];
+              $img_name=$row['img_name'];
+              $cena_podbicie=$row['cena_podbicie'];
+              echo("
+              <a href='' class='link'><div class='container-product'>
+                <img src='zdjecia/$img_name'>
+                <div class='title_description'>
+                  <div class='title'> <h3>$nazwa</h3> </div>
+                  <div class='description'> <p>$opis</p> </div>
+                </div>
+                <div class='price'>
+                    <p>$cena_podbicie zł</p>
+                </div>
+              </div></a>
+              ");
+            }
+          }
+        }else{
+          $sel=mysqli_query($connect, "SELECT *, licytacje.cena_podbicie FROM `przedmioty` JOIN licytacje ON licytacje.przedmiot_id=przedmioty.id");
+          
+          while($row = mysqli_fetch_array($sel)){
+            $nazwa=$row['nazwa'];
+            $opis=$row['opis'];
+            $img_name=$row['img_name'];
+            $cena_wywolawcza=$row['cena_wywolawcza'];
+            $cena_podbicie=$row['cena_podbicie'];
+            echo("
+            <a href='' class='link'><div class='container-product'>
+              <img src='zdjecia/$img_name'>
+              <div class='title_description'>
+                <div class='title'> <h3>$nazwa</h3> </div>
+                <div class='description'> <p>$opis</p> </div>
+              </div>
+              <div class='price'>
+                  <p>$cena_podbicie zł</p>
+              </div>
+            </div></a>
+            ");
+          }
         }
       ?>
 </section>
--- phpconfig/addauctions.php
+++ phpconfig/addauctions.php
@@ -24,9 +24,20 @@ if(isset($_REQUEST['tytul']) && isset($_REQUEST['kategoria']) && isset($_REQUEST
             move_uploaded_file($temp_name, $sciezka);
                 
             // dodawanie przedmiotu
-            $add_bid=$connect->prepare("INSERT INTO `przedmioty` VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?);");
-            $add_bid->bind_param("isssissi",$sprzedajacy_id, $tytul, $opis, $unique_imgname, $cena, $data_wystawienia, $data_zakonczenia, $kategoria);
-            $add_bid->execute();
+            $add_item=$connect->prepare("INSERT INTO `przedmioty` VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?);");
+            $add_item->bind_param("isssissi",$sprzedajacy_id, $tytul, $opis, $unique_imgname, $cena, $data_wystawienia, $data_zakonczenia, $kategoria);
+            $add_item->execute();
+            
+            $max_item_id=mysqli_query($connect,"SELECT id, cena_wywolawcza FROM `przedmioty` WHERE id=(SELECT max(id) FROM przedmioty);");
+            while($row=mysqli_fetch_array($max_item_id)){
+                $bid_item_id=$row['id'];
+                $bid_auctioner_id=null;
+                $bid_item_price=$row['cena_wywolawcza'];
+                $add_bid=$connect->prepare("INSERT INTO `licytacje` VALUES (null, ?, ?, ?);");
+                $add_bid->bind_param("ibi",$bid_item_id, $licytujacy_id, $bid_item_price);
+                $add_bid->execute();
+            }
+            
             header('Location:http://localhost/aledrogo/main/auctions.php');
         }
 
